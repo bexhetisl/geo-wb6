144ab93b1e0b65a9c390c4f2b93c0c775efc83ae
bexhetisl <bexhet.islamaj@gmail.com>
2026-04-07 03:56:01 +0200

    Delete winecad directory

--- winecad/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    #path('winecadapp', include('winecadapp.urls')),
-    path('', admin.site.urls), 
-]
-#path('', admin.site.urls),  # Admin interface
- 
